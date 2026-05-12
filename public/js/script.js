3cdb70cfd2b1998b8f84d77959a6d9a963cf4611
Vivek Sharma <viveksharma.08031994@gmail.com>
2026-05-12 18:43:25 +0530

    added chat container

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,28 @@
 let sendBtn = document.getElementById("sendBtn");
 let textBox = document.getElementById("textBox");
-sendBtn.addEventListener("click", () => {
-    let message = textBox.value;
-    alert( message);
+let chatContainer = document.getElementById("chatContainer");
+let user = { message: "" };
+
+function sendMessage(userMessage){
+let msgElement = document.createElement("div");
+msgElement.innerHTML = 
+"<span>You</span> " +
+"<span>" + userMessage + "</span>";
+chatContainer.appendChild(msgElement);
+}
+
+
+
+
+sendBtn.addEventListener("click", function (e) {
+  let userMsg = textBox.value;
+  if (userMsg == "") {
+    alert("Please enter a message");
+  } else {
+    let userMessageText = userMsg.trim();
+    user.message = userMessageText;
+    textBox.value = "";
+    sendMessage(userMessageText);
+  }
+ 
 });
