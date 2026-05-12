c4785ca662b3e636c0b498d6a2f82886398b839e
Vivek Sharma <viveksharma.08031994@gmail.com>
2026-05-12 12:50:48 +0530

    added rendring

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let ejs = require('ejs');
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+let ejs = require("ejs");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render('pages/index');
+});
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+  console.log("Server running on port 3000");
+});
